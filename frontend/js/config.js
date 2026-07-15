e24bef44971918b5ac191edd90c1e356d28a5b28
ntysgovmp-coder <ntys.gov.mp@gmail.com>
2026-07-15 09:48:13 +0800

    Delete frontend/js directory

--- frontend/js/config.js
+++
@@ -1,4 +0,0 @@
-// 部署後，把下面換成你的 Google Apps Script 網頁應用程式網址
-// (Apps Script 編輯器 -> 部署 -> 管理部署作業 -> 複製網址，格式類似：
-//  https://script.google.com/macros/s/XXXXXXXXXXXXXXXXXXXX/exec )
-const API_URL = 'https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec';
